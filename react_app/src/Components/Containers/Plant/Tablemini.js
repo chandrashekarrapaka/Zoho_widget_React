0a9e5920f9720953ee2d59c71783aeed32d1605d
chandrashekarrapaka <rapaka.chandrashekar@gmail.com>
2023-06-30 14:36:35 +0530

    leftwithKPIsApi

--- react_app/src/Components/Containers/Plant/Tablemini.js
+++ react_app/src/Components/Containers/Plant/Tablemini.js
@@ -3,8 +3,8 @@ function Tablemini(prop){
     const data=prop.data;
     // console.log("tabledata"+JSON.stringify(data));
     return(
-      <tr key={data.id} style={prop.stylz} >
-      <td>{data.name}</td><td>{data.velocityX}</td><td>{data.velocityY}</td><td>{data.velocityZ}</td><td>{data.temperature}</td><td>{data.healthScore}%</td><td>{data.trend}</td></tr>
+      <tr key={data.id}  >
+      <td style={prop.stylz}>{data.name}</td><td>{data.velocityX}</td><td>{data.velocityY}</td><td>{data.velocityZ}</td><td>{data.temperature}</td><td>{data.healthScore}%</td><td>{data.trend}</td></tr>
 
     )
 }
